e775b8b9b840b36f479110ded159af2f5841558c
dpeac-tyler <daniel.peacock@tylertech.com>
2026-06-11 11:43:32 -0800

    homepage updates

--- src/app/pages/InvoicesPage.tsx
+++ src/app/pages/InvoicesPage.tsx
@@ -544,7 +544,7 @@ export function InvoicesPage() {
                 >
                   <path
                     d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"
-                    fill="#00BDE3"
+                    fill="#1B1B1B"
                   />
                 </svg>
                 <p
